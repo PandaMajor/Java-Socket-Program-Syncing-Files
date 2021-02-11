3380309a268bd6783d03a3f032dfa4573e2ce477
Major Liu <majorliu99@gmail.com>
2021-02-10 20:58:45 -0600

    finished writing helper functions
    added comments and helper functions to generalize the process of creating and copying files from client to server sockets

--- Client.java
+++ Client.java
@@ -1,19 +1,25 @@
-// The client Role
+// The client Role (C2)
 import java.net.Socket;
 import java.io.*;
 import java.util.Arrays;
 
 public class Client {
-    // initialize socket and input output streams
+    // Initialize socket, input and output output streams
     private Socket socket = null;
     private BufferedReader in = null;
     private PrintStream out = null;
+    private String path = "C:\\Users\\major\\IdeaProjects\\socket programming\\D1\\";
 
+    /*  fileCopy reads content from a file into a byte array (of size 256)
+        send messages to ensure that content is received in correct order,
+        "content" before the actual information is sent, "received" to send
+        information. Send "EOF" message to indicate that end of file is reached.
+     */
     private void fileCopy(FileInputStream f) throws IOException {
-        byte[] b = new byte[256];
+        byte[] b = new byte[256];               // byte array used to limit message size
         String line;
         boolean eof = (f.read(b) == -1);
-        while(!eof){
+        while(!eof){                            // Repeat until EOF is reached
             out.println("content");
             out.flush();
             System.out.println("content");
@@ -22,7 +28,7 @@ public class Client {
                 out.write(b);
             }
             System.out.println((Arrays.toString(b)));
-            Arrays.fill(b, (byte) 0);
+            Arrays.fill(b, (byte) 0);           // Reset the array to 0s
             eof = (f.read(b) == -1);
             if(eof){
                 out.println("EOF");
@@ -31,14 +37,14 @@ public class Client {
         }
     }
 
-    // constructor to initialize ip address and port
-    public Client(String address, int port) throws IOException {
-        // create a connection
+    // Constructor to initialize ip address and port
+    public Client(String address, int port) {
         try{
+            // Create a connection
             socket = new Socket(address, port);
             System.out.print("Connected");
 
-            // create input/output channels
+            // Create input/output channels
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintStream(socket.getOutputStream());
 
@@ -48,53 +54,46 @@ public class Client {
             out.println("D1copy");
             out.flush();
             line = in.readLine();
+            // Upon receiving "D1copy made", send message to create F1
             if(line.equals("D1copy made")){
                 out.println("make F1");
                 out.flush();
                 line = in.readLine();
+                // Upon receiving "F1 made", send message to prepare copying F1
                 if(line.equals("F1 made")){
-                    //byte[] b = new byte[256];
-                    FileInputStream f = new FileInputStream("C:\\Users\\major\\IdeaProjects\\socket programming\\D1\\F1.txt");
+                    FileInputStream f = new FileInputStream(path+"F1.txt");
                     fileCopy(f);
-                    /*boolean eof = (f.read(b) == -1);
-                    while(!eof){
-                        out.println("content");
-                        out.flush();
-                        System.out.println("content");
-                        line = in.readLine();
-                        if(line.equals("received")){
-                            out.write(b);
-                        }
-                        System.out.println((Arrays.toString(b)));
-                        Arrays.fill(b, (byte) 0);
-                        eof = (f.read(b) == -1);
-                        if(eof){
-                            out.println("EOF");
-                            System.out.println("EOF");
-                        }
-                    }*/
                     line = in.readLine();
+                    // Upon receiving "F1 finished", send message to create F2
                     if(line.equals("F1 finished")){
-                        f = new FileInputStream("C:\\Users\\major\\IdeaProjects\\socket programming\\D1\\F2.txt");
+                        f = new FileInputStream(path+"F2.txt");
+                        out.println("make F2");
+                        out.flush();
+                        line = in.readLine();
+                        // Upon receiving "F2 made", send message to prepare copying F2
+                        if(line.equals("F2 made")){
+                            fileCopy(f);
+                            line = in.readLine();
+                            // Upon receiving "F2 finished", send message to signal end of session
+                            if(line.equals("F2 finished")){
+                                out.println("end of session");
+                                out.flush();
+                                // close connection
+                                in.close();
+                                out.close();
+                                socket.close();
+                            }
+                        }
                     }
                 }
             }
         }
-        catch(IOException i){
-            System.out.println(i);
-        }
-
-        // close the connection
-        try{
-            in.close();
-            out.close();
-            socket.close();
-        }
-        catch(IOException i){
+        catch(IOException i) {
             System.out.println(i);
         }
     }
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
         Client client = new Client("127.0.0.1", 5000);
     }
 }
--- Server.java
+++ Server.java
@@ -1,17 +1,56 @@
-// The server role
+// The server role (C1)
 import java.net.*;
 import java.io.*;
 
 public class Server {
-    //initialize socket and input stream
+    // Initialize socket, server, and input and output streams
     private Socket socket = null;
     private ServerSocket server = null;
     private BufferedReader in = null;
     private PrintWriter out = null;
+    private String path = "C:\\Users\\major\\IdeaProjects\\socket programming\\D1copy\\";
 
-    // constructor with port
+    /*  makeFile creates the file with the file name that's passed as an argument
+        returns boolean depending on if the file was able to be created
+     */
+    private boolean makeFile(String filename) throws IOException {
+        File file = new File(path + filename);
+        if(file.createNewFile()) {
+            out.println(filename + " made");
+            out.flush();
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /*  copyFile copies input from the client as a byte array (of size 256).
+        The function receives packets one at a time by acknowledging receiving
+        packets by responding with "received". Packets are preceded "content"
+        labels to guarantee that they are received in order. This continues until
+        the "EOF" is received.
+     */
+    private void copyFile(File file) throws IOException {
+        String line = "";
+        OutputStream os = new FileOutputStream(file);
+        DataInputStream temp = new DataInputStream(socket.getInputStream());
+        line = in.readLine();
+        while(!line.equals("EOF")) {
+            byte[] b = new byte[256];
+            if (line.equals("content")) {
+                out.println("received");
+                out.flush();
+                temp.read(b);
+                os.write(b);
+            }
+            line = in.readLine();   // Check if EOF is reached
+        }
+    }
+
+    // Constructor with port
     public Server(int port){
-        // start server and wait for connection
+        // Start server and wait for connection
         try{
             server = new ServerSocket(port);
             System.out.println("Server started");
@@ -19,54 +58,60 @@ public class Server {
             socket = server.accept();
             System.out.println("Client accepted");
 
-            // create input/output channels
+            // Create input/output channels
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out =  new PrintWriter(socket.getOutputStream());
 
+            // line is used to read messages sent from client
             String line = in.readLine();
-            if(line.equals("D1copy")){
+
+            if(line.equals("D1copy")){              // Upon receiving "D1copy", try to make folder D1copy
                 File file = new File(line);
                 boolean bool = file.mkdir();
                 if(bool){
-                    System.out.println("Directory created successfully");
-                    out.println("D1copy made");
+                    String filename;
+                    out.println("D1copy made");     // Acknowledge the creation of the directory
                     out.flush();
                     line = in.readLine();
-                    if(line.equals("make F1")){
-                        file = new File("C:\\Users\\major\\IdeaProjects\\socket programming\\D1copy\\F1");
-                        if(file.createNewFile()){
-                            out.println("F1 made");
+                    if(line.equals("make F1")){     // Create file F1 and copy information from C2
+                        filename = "F1";
+                        if(makeFile(filename)){     // Send message acknowledging completion of copying
+                            copyFile(new File(path+filename));
+                            out.println(filename + " finished");
                             out.flush();
-                            OutputStream os = new FileOutputStream(file);
-                            DataInputStream temp = new DataInputStream(socket.getInputStream());
-                            line = in.readLine();
-                            while(!line.equals("EOF")) {
-                                byte[] b = new byte[256];
-                                if(line.equals("content")){
-                                    out.println("received");
-                                    out.flush();
-                                    temp.read(b);
-                                    os.write(b);
-                                }
-                                line = in.readLine();
-                            }
-                            out.println("F1 finshed");
-
                         }
                         else{
-                            System.out.println("error");
+                            System.out.println("File " + filename + " already exists.");
+                        }
+                    }
+                    line = in.readLine();
+                    if(line.equals("make F2")){     // Create file F2 and copy information from C2
+                        filename = "F2";
+                        if(makeFile(filename)){     // Send message acknowledging completion of copying
+                            copyFile(new File(path+filename));
+                            out.println(filename + " finished");
+                            out.flush();
+                        }
+                        else{
+                            System.out.println("File " + filename + " already exists.");
                         }
                     }
                 }
                 else {
-                    System.out.println("Couldn't create file");
+                    // Folder could not be created, so close connection
+                    System.out.println("Couldn't create folder");
+                    out.close();
+                    in.close();
+                    socket.close();
                 }
             }
-
-            // close connection
-            out.close();
-            in.close();
-            socket.close();
+            line = in.readLine();
+            if(line.equals("end of session")){
+                // Close connection
+                out.close();
+                in.close();
+                socket.close();
+            }
         }
         catch(IOException i){
             System.out.println(i);
